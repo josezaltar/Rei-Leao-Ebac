124c1e0ba74f269d3028e1b4b4ad06a678afdfe7
zezaltar <josezaltar@gmail.com>
2025-06-04 14:43:22 -0300

    Corrigindo o site

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'));
+  return gulp
+    .src("./src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("./dist/css"));
 }
 
 function images() {
-    return gulp.src(['./src/images/**/*'], { encoding: false })
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+  return gulp
+    .src(["./src/images/**/*"], { encoding: false })
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(styles, images,);
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-}
+exports.default = gulp.parallel(styles, images);
+exports.watch = function () {
+  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+};
